3f3e1441827c13688b366eaf2006687fc3ad0c29
Leona044 <45042289+Leona044@users.noreply.github.com>
2019-09-18 23:58:17 +0200

    utf-8 pokušaj_2

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,17 @@
 <?php include("skripta.php"); ?>
 
+<!DOCTYPE html>
+<html lang="hr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>UNIPU Shop</title>
+    <link rel="stylesheet" href="styles/bootstrap-337.min.css">
+    <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+
+<body>
 <div class="panel panel-default sidebar-menu"><!-- panel panel-default sidebar-menu Begin -->
     <div class="panel-heading"><!-- panel-heading Begin -->
         <h3 class="panel-title">Kategorije proizvoda</h3>
@@ -29,3 +41,6 @@
         </ul><!-- nav nav-pills nav-stacked category-menu Finish -->
     </div><!-- panel-body Finish -->
 </div><!-- panel panel-default sidebar-menu Finish -->
+
+</body>
+</html>
